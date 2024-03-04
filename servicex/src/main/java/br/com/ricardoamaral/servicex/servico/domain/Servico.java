8836f01c35783029e94a496185a75976a9151825
RicardoAugusto <2022210220004@iesp.edu>
2024-03-04 20:36:15 -0300

    terceiro  commit

---
+++ servicex/src/main/java/br/com/ricardoamaral/servicex/servico/domain/Servico.java
@@ -0,0 +1,13 @@
+package br.com.ricardoamaral.servicex.servico.domain;
+
+import br.com.ricardoamaral.servicex.categoria.domain.Categoria;
+
+import java.util.List;
+
+public class Servico {
+    private  Integer idServico;
+    private  String nome;
+    private Double valor;
+    private List<Categoria> categoria;
+
+}

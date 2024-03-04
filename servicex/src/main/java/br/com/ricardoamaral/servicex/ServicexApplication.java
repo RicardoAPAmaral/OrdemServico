573fa85a46eeef4ac41c372c6b2350f784fc027f
RicardoAugusto <2022210220004@iesp.edu>
2024-03-04 19:03:09 -0300

    segundo  commit

--- servicex/src/main/java/br/com/ricardoamaral/servicex/ServicexApplication.java
+++ servicex/src/main/java/br/com/ricardoamaral/servicex/ServicexApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServicexApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ServicexApplication.class, args);
 	}
